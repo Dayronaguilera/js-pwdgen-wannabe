a2ddfd6a455e21f6ecd1e20cb92b130dd7a6adda
Dayronaguilera <aguilera.dayron@gmail.com>
2021-05-27 16:18:03 +0200

    Sicuramente fuori strada

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,3 +1,10 @@
 // delle prove con imput
 
-var 
+var fname = document.getElementById(fname);
+var lname = document.getElementById(lname);
+var color = document.getElementById(color);
+
+function myfunction(params) {
+    var password = fname + lname + color;
+    
+}
